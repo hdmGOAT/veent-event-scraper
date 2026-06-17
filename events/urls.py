b87482e3f87d4b49d3b5c39d3ca35bf8236570c2
msallsundaee <bea.bacaling2@gmail.com>
2026-06-17 09:06:31 +0800

    ui design across pages

--- events/urls.py
+++ events/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "events"
 
 urlpatterns = [
-    path("", views.event_list, name="event_list"),
-    path("dashboard/", views.dashboard, name="dashboard"),
+    path("", views.dashboard, name="dashboard"),
+    path("events/", views.event_list, name="event_list"),
     path("events/<slug:slug>/", views.event_detail, name="event_detail"),
     path("venues/", views.venue_list, name="venue_list"),
     path("venues/<slug:slug>/", views.venue_detail, name="venue_detail"),
